bc7172a3ac4632550b612af2f149199f257acfc0
Арслан <47584980+15170027@users.noreply.github.com>
2019-04-02 21:02:46 +0500

    Update HelloWorld.java

--- src/main/java/HelloWorld/HelloWorld/HelloWorld.java
+++ src/main/java/HelloWorld/HelloWorld/HelloWorld.java
@@ -1,13 +1,7 @@
-package HelloWorld.HelloWorld;
+package example;
 
-/**
- * Hello world!
- *
- */
-public class HelloWorld 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Calc {
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
